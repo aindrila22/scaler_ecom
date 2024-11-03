5f7778c6e6c77bc1574e419833f9b81b62a32817
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-11-03 20:19:08 +0530

    url error fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
+// Fix for ESM modules, replacing __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
