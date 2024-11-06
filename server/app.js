6eeea51219c78bfb126b7ae474c5d7cd8230f6a6
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-11-06 11:24:33 +0530

    cors update

--- server/app.js
+++ server/app.js
@@ -1,42 +1,46 @@
-// server.js
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const authRoutes = require('./routes/auth/register');
-const loginRoutes = require('./routes/auth/login');
-const userRoutes = require('./routes/auth/user');
-const uploadRoutes = require('./routes/upload');
-const checkoutRoutes = require('./routes/checkout');
-const orderRoutes = require('./routes/order');
-const webhook = require('./routes/webhook');
+const authRoutes = require("./routes/auth/register");
+const loginRoutes = require("./routes/auth/login");
+const userRoutes = require("./routes/auth/user");
+const uploadRoutes = require("./routes/upload");
+const checkoutRoutes = require("./routes/checkout");
+const orderRoutes = require("./routes/order");
+const webhook = require("./routes/webhook");
 
 //const refreshRoute = require('./routes/auth/refreshToken');
 
-
+const corsOptions = {
+  origin: `${process.env.FRONTEND_URL}`,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/auth', userRoutes);
-app.use('/auth', loginRoutes);
-app.use('/file', uploadRoutes);
-app.use('/api', checkoutRoutes);
-app.use('/api', orderRoutes);
+app.use("/auth", authRoutes);
+app.use("/auth", userRoutes);
+app.use("/auth", loginRoutes);
+app.use("/file", uploadRoutes);
+app.use("/api", checkoutRoutes);
+app.use("/api", orderRoutes);
 app.use(webhook);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
