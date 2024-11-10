7d241411e1ab18b17d98b2292b13ca141a7d6fe1
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-11-10 13:54:42 +0530

    performance optimize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,45 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./components/Home/LandingPage";
-import Signup from "./components/auth/Signup";
-import NotFound from "./components/NotFound";
+import { Suspense, lazy } from "react";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Upload from "./components/configure/Upload";
-import Design from "./components/configure/Design";
-import Preview from "./components/configure/Preview";
-import Login from "./components/auth/Login";
-import Success from "./components/result/Success";
-import Cancel from "./components/result/Cancel";
-import Dashboard from "./components/admin/Dashboard";
-import OrderDetails from "./components/admin/OrderDetails";
-import OrderStatus from "./components/status/OrderStatus";
-import Profile from "./components/status/Profile";
+
+// Use lazy loading for routes
+const LandingPage = lazy(() => import("./components/Home/LandingPage"));
+const Signup = lazy(() => import("./components/auth/Signup"));
+const NotFound = lazy(() => import("./components/NotFound"));
+const Upload = lazy(() => import("./components/configure/Upload"));
+const Design = lazy(() => import("./components/configure/Design"));
+const Preview = lazy(() => import("./components/configure/Preview"));
+const Login = lazy(() => import("./components/auth/Login"));
+const Success = lazy(() => import("./components/result/Success"));
+const Cancel = lazy(() => import("./components/result/Cancel"));
+const Dashboard = lazy(() => import("./components/admin/Dashboard"));
+const OrderDetails = lazy(() => import("./components/admin/OrderDetails"));
+const OrderStatus = lazy(() => import("./components/status/OrderStatus"));
+const Profile = lazy(() => import("./components/status/Profile"));
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/configure/upload" element={<Upload />} />
-        <Route path="/configure/design/:id" element={<Design />} />
-        <Route path="/configure/preview/:id" element={<Preview />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/orders/:id" element={<OrderDetails />} />
-        <Route path="/customcase/orders/:id" element={<OrderStatus />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/configure/upload" element={<Upload />} />
+          <Route path="/configure/design/:id" element={<Design />} />
+          <Route path="/configure/preview/:id" element={<Preview />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/cancel" element={<Cancel />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/orders/:id" element={<OrderDetails />} />
+          <Route path="/customcase/orders/:id" element={<OrderStatus />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   );
